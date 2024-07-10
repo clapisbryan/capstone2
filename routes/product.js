5e7ae4569adb8ac1aa01c108177d0b76a40e8c83
clapisbryan <clapisbryan@gmail.com>
2024-07-10 13:11:31 +0800

    change routes link

--- routes/product.js
+++ routes/product.js
@@ -6,13 +6,13 @@ const { verify, verifyAdmin } = require("../auth");
 
 const router = express.Router();
 
-router.post("/products", verify, verifyAdmin, productController.getProducts); // Create New Product (admin only)
-router.get("/products", productController.retrieveAllproduct); // Retrieves all products.
-router.get("/products/active", productController.retrieveActiveproduct); // Retrieves active products.
-router.get("/products/:id", verify, productController.retrieveProductById); // Retrieves a single product by ID
-router.put("/products/:id", verify, verifyAdmin, productController.updateProductById); // Updates a product by ID (admin only)
-router.put("/products/:id/archive", verify, verifyAdmin, productController.archiveProduct); // Archives a product by ID (admin only)
-router.put("/products/:id/activate"); // Activates a product by ID (admin only)
+router.post("/", verify, verifyAdmin, productController.getProducts); // Create New Product (admin only)
+router.get("/all", productController.retrieveAllproduct); // Retrieves all products.
+router.get("/active", productController.retrieveActiveproduct); // Retrieves active products.
+router.get("/:productsid", verify, productController.retrieveProductById); // Retrieves a single product by ID
+router.patch("/:productsid/update", verify, verifyAdmin, productController.updateProductById); // Updates a product by ID (admin only)
+router.patch("/:productsid/archive", verify, verifyAdmin, productController.archiveProduct); // Archives a product by ID (admin only)
+router.patch("/:productsid/activate"); // Activates a product by ID (admin only)
 
 router.post(
   "/products",
