c27126bd5358bacdcf3456970aecd0df531ac3d7
clapisbryan <clapisbryan@gmail.com>
2024-07-10 15:37:35 +0800

    created orders for s55

--- models/Order.js
+++ models/Order.js
@@ -1,21 +1,13 @@
 const mongoose = require("mongoose");
 
-// const User = require("../models/User");
-
-// const Product = require("../models/Product");
-
 const orderSchema = new mongoose.Schema({
   userId: {
-    // type: mongoose.Schema.Types.ObjectId, // userId refers to the User model's ObjectId
-    // ref: 'User', // Reference to the User model
     type: String,
     required: [true, "User ID is required"],
   },
   productsOrdered: [
     {
       productId: {
-        // type: mongoose.Schema.Types.ObjectId, // productId refers to the Product model's ObjectId
-        // ref: 'Product', // Reference to the Product model
         type: String,
         required: [true, "Product ID is required"],
       },
