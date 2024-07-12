04e6c91e9d87b06723ecc7a6c8cdc55103e76820
cindygaoat.cg <cindygaoat.cg@gmail.com>
2024-07-12 08:32:15 +0800

    add order resources

---
+++ controllers/order.js
@@ -0,0 +1,26 @@
+const Order = require("../models/Order");
+const { errorHandler } = require("../auth");
+
+module.exports.retrieveAllOrder = (req, res) => {
+	return Order.Find({})
+	.then((order) => {
+		if(!order) {
+			return res.status(404).send({ message: "No Orders Found"})
+		} else {
+			return res.status(200).send({ orders: order})
+		}
+	})
+}
+
+module.exports.retrieveMyOrder = (req, res) => {
+
+	return Order.FindOne({ userId : req.user.id })
+	.then((order) => {
+		if(!order) {
+			return res.status(404).send({ message: "No Orders Found"})
+		} else {
+			return res.status(200).send({ orders: order})
+		}
+	})
+	.catch((err) => errorHandler(err, req, res))
+}
---
+++ routes/order.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const orderController = require("../controllers/order");
+
+const { verify, verifyAdmin } = require("../auth");
+
+const router = express.Router();
+
+router.get("/my-orders", verify, orderController.retrieveMyOrder);
+router.get("/all-orders", verify, verifyAdmin, orderController.retrieveAllOrder);
+module.exports = router;
