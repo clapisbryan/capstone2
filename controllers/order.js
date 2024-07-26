6ac0fb2bcaa32cf134c4187dd98d74406351c6ea
unknown <clapisbryan@gmail.com>
2024-07-26 11:19:05 +0800

    add new updated product on create order

--- controllers/order.js
+++ controllers/order.js
@@ -5,38 +5,52 @@ module.exports.createOrder = async (req, res) => {
   const { userId, productsOrdered, totalPrice } = req.body;
 
   if (!userId || !productsOrdered || !totalPrice) {
-    return res.status(400).send({
-      message: "UserId, productsOrdered, and totalPrice are required",
-    });
+      return res.status(400).send({
+          message: "UserId, productsOrdered, and totalPrice are required",
+      });
   }
 
   if (productsOrdered.length === 0) {
-    return res.status(400).send({ message: "No items to checkout" });
+      return res.status(400).send({ message: "No items to checkout" });
   }
 
-  return await Order.findOne({ userId: userId })
-    .then((orderExist) => {
-      if (orderExist) {
-        return res.status(400).send({ message: "Order already exist" });
-      } else {
-        const newOrder = new Order({
+  try {
+      // Check if an order already exists for the user
+      const existingOrder = await Order.findOne({ userId: userId });
+
+      if (existingOrder) {
+          // Update the existing order with new items
+          existingOrder.productsOrdered = productsOrdered;
+          existingOrder.totalPrice = totalPrice;
+          const updatedOrder = await existingOrder.save();
+
+          return res.status(200).send({
+              message: "Order updated successfully",
+              orderId: updatedOrder._id
+          });
+      }
+
+      // Create and save the new order
+      const newOrder = new Order({
           userId: userId,
           productsOrdered: productsOrdered,
           totalPrice: totalPrice,
-        });
+      });
 
-        return newOrder
-          .save()
-          .then((result) =>
-            res.status(201).send({
-              message: "Ordered successfully",
-            })
-          )
-          .catch((err) => errorHandler(err, req, res));
-      }
-    })
-    .catch((err) => errorHandler(err, req, res));
+      const result = await newOrder.save();
+      res.status(201).send({
+          message: "Order created successfully",
+          orderId: result._id
+      });
+  } catch (err) {
+      return res.status(500).send({
+          message: "An error occurred while creating or updating the order",
+          error: err.message
+      });
+  }
 };
+
+
 module.exports.retrieveAllOrder = async (req, res) => {
   return await Order.find({}).then((order) => {
     if (!order) {
